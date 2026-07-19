22796e58caa8854db079427ad974060d5956f27c
Spockfish <htenberge@gmail.com>
2026-07-19 21:14:02 +0200

    use https instead of git

--- package/qt5/qt5quickcontrols2/qt5quickcontrols2.mk
+++ package/qt5/qt5quickcontrols2/qt5quickcontrols2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-QT5QUICKCONTROLS2_VERSION = $(shell git ls-remote git://code.qt.io/qt/qtquickcontrols2.git 5.15 | awk '{ print $$1 }')
+QT5QUICKCONTROLS2_VERSION = $(shell git ls-remote https://code.qt.io/qt/qtquickcontrols2.git 5.15 | awk '{ print $$1 }')
 QT5QUICKCONTROLS2_SITE = $(QT5_SITE)/qtquickcontrols2.git
 QT5QUICKCONTROLS2_SITE_METHOD = git
 QT5QUICKCONTROLS2_DEPENDENCIES = qt5declarative
