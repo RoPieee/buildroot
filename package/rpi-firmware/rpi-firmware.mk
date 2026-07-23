5851f214267a588f7dd825772b3a6a7c4410746d
Spockfish <htenberge@gmail.com>
2026-07-23 20:57:59 +0200

    bump package rpi-firmware

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 # Please keep in sync with configs/raspberrypi*_deconfig
-RPI_FIRMWARE_VERSION = ed05d403048f2956d9d3653acd996157363e94fe
+RPI_FIRMWARE_VERSION = 8402891f3ffba8a2f1fb8b975bccb9a2e5c01335
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
